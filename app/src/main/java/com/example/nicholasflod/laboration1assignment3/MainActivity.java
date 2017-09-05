44abc0fd38b8231139faf9833e7611a507022bb2
flodare <Nicholas.flod@gmail.com>
2017-09-05 13:22:51 +0200

    Test

--- app/src/main/java/com/example/nicholasflod/laboration1assignment3/MainActivity.java
+++ app/src/main/java/com/example/nicholasflod/laboration1assignment3/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.nicholasflod.laboration1assignment3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.*;
+import android.hardware.*;
+import android.util.Log;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SensorActivity sensorObjekt = new SensorActivity();
+    }
+}
+
+class SensorActivity extends Activity implements SensorEventListener {
+    private static final String TAG = "MyActivity";
+    private final SensorManager mSensorManager;
+    private final Sensor mAccelerometer;
+
+    public SensorActivity() {
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+    }
+
+    public void onSensorChanged(SensorEvent event)
+    {
     }
 }
