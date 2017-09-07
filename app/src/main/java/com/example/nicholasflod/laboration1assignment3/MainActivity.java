28dd6c3a6a5d791ed0d0d7e9617e4c0480e0d1e9
flodare <Nicholas.flod@gmail.com>
2017-09-07 13:45:17 +0200

    Can now track phones xyz

--- app/src/main/java/com/example/nicholasflod/laboration1assignment3/MainActivity.java
+++ app/src/main/java/com/example/nicholasflod/laboration1assignment3/MainActivity.java
@@ -1,17 +1,65 @@
 package com.example.nicholasflod.laboration1assignment3;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.io.Console;
+import android.util.Log;
+import android.app.*;
+import android.hardware.*;
+import android.util.Log;
+import android.content.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private float last_x, last_y, last_z;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Log.d("onCreate", "Initierat sensorerna");
 
+    }
+
+    protected void onResume()
+    {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause()
+    {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+
+    }
+
+    public final void onSensorChanged(SensorEvent event)
+    {
+        Sensor mySensor = event.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+                last_x = x;
+                Log.d("sensor", Float.toString(x));
+                last_y = y;
+                Log.d("sensor", Float.toString(y));
+                last_z = z;
+                Log.d("sensor", Float.toString(z));
+
+        }
     }
 }
 
